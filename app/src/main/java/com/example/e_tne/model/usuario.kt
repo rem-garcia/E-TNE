7ee07b8fcaa8fa2da617e3b6514eed505b408d4c
rem.garcia <rem.garcia@duocuc.cl>
2025-10-19 02:37:01 -0300

    se crean el primer commit, los primeros package y la clase usuario

---
+++ app/src/main/java/com/example/e_tne/model/usuario.kt
@@ -0,0 +1,11 @@
+package com.example.e_tne.model
+
+class Usuario {
+    val nombre: String=""
+    val  apepPaterno: String=""
+    val apeMaterno: String= ""
+    val rut: Int=0
+    val dv: String=""
+
+
+}
